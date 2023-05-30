7632239dbbd1ba4dd469a4c8a78c0bc4e05a1fbf
Ἀρτεμής <ivanivanov31922@gmail.com>
2023-05-30 12:19:56 +0300

    init

---
+++ app/src/main/java/com/example/dormitoryapp/view/fragment/SplashFragment.kt
@@ -0,0 +1,41 @@
+package com.example.dormitoryapp.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.dormitoryapp.R
+import com.example.dormitoryapp.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : Fragment() {
+
+    private val binding: FragmentSplashBinding by lazy {
+        FragmentSplashBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            ivLogo.animate()
+                .alpha(1f).duration = 1500
+            lifecycleScope.launch {
+                delay(1500)
+                findNavController().navigate(R.id.action_splashFragment_to_onboardingFragment)
+            }
+        }
+    }
+
+}
